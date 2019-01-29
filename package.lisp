b5d91a6b64852f9f3ea62cce2785ed1750f2f954
Michał "phoe" Herda <phoe@disroot.org>
2019-01-29 22:17:33 +0100

    portabilize tests

--- package.lisp
+++ package.lisp
@@ -5,8 +5,29 @@
   (:import-from
    #+sbcl #:sb-ext
    #+ccl  #:ccl
-   #+ecl ext
+   #+ecl  #:ext
+   #+abcl #:ext
    #:package-local-nicknames
    #:package-locally-nicknamed-by-list
    #:add-package-local-nickname
    #:remove-package-local-nickname))
+
+(in-package #:package-local-nicknames-tests)
+
+(progn
+  (defparameter +test-data+
+    #+sbcl '(:sb-ext :sb :exit)
+    #+ccl  '(:ccl    :cc :quit)
+    #+ecl  '(:ext    :ex :exit)
+    #+abcl '(:ext    :ex :quit))
+
+  (defparameter +pkg-name+ (first +test-data+))
+  (defparameter +nn-name+ (second +test-data+))
+  (defparameter +sym-name+ (third +test-data+))
+
+  (defparameter +pkg-sname+ (string +pkg-name+))
+  (defparameter +nn-sname+ (string +nn-name+))
+  (defparameter +sym-sname+ (string +sym-name+))
+  (defparameter +sym-fullname+ (concatenate 'string +pkg-sname+ ":" +sym-sname+))
+  (defparameter +sym-fullnickname+ (concatenate 'string +nn-sname+ ":" +sym-sname+))
+  (defparameter +sym+ (find-symbol +sym-sname+ +pkg-name+)))
